713d627b01fc94870718555aff4e9fdd41ede853
Mario Estrada <mario.estrada@gmail.com>
2012-03-13 17:09:51 -0500

    added missing Loader.php

---
+++ app/framework/Cr/Loader.php
@@ -0,0 +1,30 @@
+<?php
+
+class Cr_Loader{
+	
+	protected $_root_dir = '.';
+	
+	public function __construct($root_dir = 'app/framework/')
+	{
+		$this->_root_dir = $root_dir;
+
+		set_include_path($root_dir . PATH_SEPARATOR . get_include_path());
+		
+		spl_autoload_register(array(__CLASS__, 'load'));
+	}
+	
+	public function load($class_name)
+	{
+		if(class_exists($class_name, false) || interface_exists($class_name, false))
+		{
+			return true;
+		}
+		
+		try{
+			require_once str_replace('_', '/', $class_name) . '.php';
+		}catch(Exception $e){
+		}
+		
+		return class_exists($class_name, false) || interface_exists($class_name, false);
+	}
+}
